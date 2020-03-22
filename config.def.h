67daac468162429e41479ec7792bfecf41224cfd
Jim Read <jimmydean886@hotmail.com>
2020-03-22 10:57:50 -0400

    Changed to Fira Code

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx        = 0;        /* border pixel of windows
 static const unsigned int snap            = 32;       /* snap pixel */
 static const int showbar                  = 1;        /* 0 means no bar */
 static const int topbar                   = 1;        /* 0 means bottom bar */
-static const char *fonts[]                = { "Fira Mono:size=9" };
-static const char dmenufont[]             = "Fira Mono:size=9";
+static const char *fonts[]                = { "Fira Code:size=9" };
+static const char dmenufont[]             = "Fira Code:size=9";
 static       char col_grey1[]             = "#222222";
 static       char col_grey2[]             = "#444444";
 static       char col_grey3[]             = "#bbbbbb";
