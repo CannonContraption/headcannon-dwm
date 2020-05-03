629a731fe7d19d9c3b1b8454ed5c6186dd61ad08
Jim Read <jimmydean886@hotmail.com>
2020-05-03 00:25:44 -0400

    Make example window rules a comment
    The example window rules are now commented out, since in reality the
    usefulness of these rules is highly individual

--- config.def.h
+++ config.def.h
@@ -24,9 +24,9 @@ static const Rule rules[] =
     *	WM_NAME(STRING) = title
     */
    /* class      instance    title       tags mask     isfloating   monitor */
-   { "Gimp",     NULL,       NULL,       0,            0,           -1 },
-   { "Firefox",  NULL,       NULL,       0,            0,           -1 },
-   { "keepassxc",NULL,       NULL,       1>>10,        1,           -1 },
+   /* { "Gimp",     NULL,       NULL,       0,            0,           -1 }, */
+   /* { "Firefox",  NULL,       NULL,       0,            0,           -1 }, */
+   /* { "keepassxc",NULL,       NULL,       1>>10,        1,           -1 }, */
   };
 
 /* layout(s) */
